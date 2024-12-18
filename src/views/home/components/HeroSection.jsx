843b4f6a0f3f635e120f9b690fd0008d09202312
Ahmad Nazirul <nazirul777@yahoo.co.id>
2024-12-18 10:15:10 +0700

    Update HeroSection.jsx

--- src/views/home/components/HeroSection.jsx
+++ src/views/home/components/HeroSection.jsx
@@ -3,15 +3,12 @@ const HeroSection = (props) => (<div className="vh-60">
         <div className="row py-5">
             <div className="col-md-6 text-center text-md-start">
                 <h1 className="text-heading-hero">
-                    On overcoming obstacles
+                   Ahooy
                 </h1>
                 <p className="text-p-hero px-5 px-lg-0">
-                    Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.
+                    Text disini
                 </p>
                 {props.children}
-            </div>
-            <div className="col-md-6 vh-50-max d-none d-md-block">
-                <img className="img-fluid br-20" src="/images/img-1.jpg" alt="exam" />
             </div>
         </div>
     </div >
